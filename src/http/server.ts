5e1d36f59e42dfea74609b32695534713ed88fc2
Franklin Ramos <franklinramos@outlook.com>
2024-02-07 19:02:29 -0300

    Add getPoll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { createPolls } from './routes/create-polls'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 
 app.register(createPolls)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
