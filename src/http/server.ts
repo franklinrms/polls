2788e5e464e8fd75a2e62d682c795b139b32a2d2
Franklin Ramos <franklinramos@outlook.com>
2024-02-07 20:28:32 -0300

    Add @fastify/cookie and register cookie plugin in server.ts

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { createPolls } from './routes/create-polls'
 import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'a1713018-d01f-40fc-939a-59f15b188f1f',
+  hook: 'onRequest',
+})
+
 app.register(createPolls)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
