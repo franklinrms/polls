18a761a1710baddd2798cc4d6f03db96c649664a
Franklin Ramos <franklinramos@outlook.com>
2024-02-07 21:27:40 -0300

    Add @fastify/websocket package and implement WebSocket functionality

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify'
+import websocket from '@fastify/websocket'
 import cookie from '@fastify/cookie'
 
 import { createPolls } from './routes/create-polls'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
+
+app.register(websocket)
 
 app.register(cookie, {
   secret: 'a1713018-d01f-40fc-939a-59f15b188f1f',
@@ -15,6 +19,7 @@ app.register(cookie, {
 app.register(createPolls)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
